f7165850fcc0bc79a2a1c7eca079174cfa693d31
Rafał Rogowski <rogowski.r@live.com>
2020-04-01 12:18:11 +0200

    Dodaj pliki projektów.

---
+++ SKRMConn/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SKRMConn
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        readonly MysqlConn Conn1 = new MysqlConn("Server=mysql-727668.vipserv.org;Database=rafik73_mapy;Uid=rafik73_mapy;Pwd=Rafaello73;");
+       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> xx = Conn1.DoConn("event","SELECT * FROM events ORDER BY event ASC");
+            foreach (string a in xx)
+            {
+                comboBox.Items.Add(a);
+            }
+             //   textBox1.Text = Conn1.DoConn("SELECT * FROM events");
+        }
+    }
+}
---
+++ SKRMConn/MysqlConn.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows;
+
+namespace SKRMConn
+{
+    class MysqlConn
+    {
+        //string dataFromQuery;
+        readonly string skrmConnString;
+        readonly List<string> skrmDataList = new List<string>();
+
+        public MysqlConn(string skrmConnString)
+        {
+            this.skrmConnString = skrmConnString;
+        }
+
+        public List<string> DoConn(string columnName , string sql)
+        {
+             MySqlConnection SkrmConn = new MySqlConnection(skrmConnString);
+            try
+            {
+                SkrmConn.Open();
+                MySqlCommand Cmd = new MySqlCommand(sql, SkrmConn);
+                MySqlDataReader rdr = Cmd.ExecuteReader();
+
+                // while (rdr.Read())
+                // {
+                //    win.textBox1.Text = rdr.GetString(1);
+                // }
+
+                while (rdr.Read())
+                {
+                   //dataFromQuery += rdr.GetValue(1).ToString();
+                    //skrmDataList.Add(rdr.GetValue(columnNumber).ToString());
+                    skrmDataList.Add(rdr.GetString(columnName).ToString());
+                }
+
+
+               // MessageBox.Show("ServerVersion: " + SkrmConn.ServerVersion +
+               // "\nState: " + SkrmConn.State.ToString() + "\nDatabase: " + SkrmConn.Database);
+               SkrmConn.Close();
+            }
+            catch (Exception connErr)
+            {
+                
+                MessageBox.Show(connErr.ToString());
+               
+            }
+            return skrmDataList;
+        }
+
+    }
+}
